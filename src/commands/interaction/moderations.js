c4bc288e51b106e25d58af87d13e25c54ec0367c
duo <106779045+4y6f@users.noreply.github.com>
2026-05-01 22:08:40 -0700

    added more utility commands.

---
+++ src/commands/interaction/moderations.js
@@ -0,0 +1,176 @@
+const settings = require('../../config/settings');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+const { hasPermission, permissionReply } = require('../../utils/permissionChecker');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('moderation')
+        .setDescription('Moderation commands')
+        .addSubcommand((sub) =>
+            sub
+            .setName('warning')
+            .setDescription('Issue a warning to a user')
+            .addUserOption(option => option.setName('target').setDescription('The user to warn').setRequired(true))
+            .addStringOption((option) => option.setName('reason').setDescription('What are you warning this user for?').setRequired(true))
+        )
+        .addSubcommand((sub) =>
+            sub
+            .setName('timeout')
+            .setDescription('Timeout a user')
+            .addUserOption((option) => option.setName('target').setDescription('The user to be timed out').setRequired(true))
+            .addStringOption((option) => option.setName('reason').setDescription('What are you timing out this user for?').setRequired(true))
+            .addStringOption((option) => 
+                option
+                .setName('duration')
+                .setDescription('Duration of the timeout in minutes (max 24 hours)')
+                .setRequired(true)
+                .addChoices(
+                    {
+                        name: '10 minutes',
+                        value: '10',
+                    },
+                    {
+                        name: '30 minutes',
+                        value: '30',
+                    },
+                    {
+                        name: '1 hour',
+                        value: '60',
+                    },
+                    {
+                        name: '12 hours',
+                        value: '720',
+                    },
+                    {
+                        name: '24 hours',
+                        value: '1440',
+                    }
+            )))
+        .addSubcommand((sub) => 
+            sub
+            .setName('kick')
+            .setDescription('Kick a user from the server')
+            .addUserOption((option) => option.setName('target').setDescription('The user to kick').setRequired(true))
+            .addStringOption((option) => option.setName('reason').setDescription('Why are you kicking this user?').setRequired(true))
+        )
+        .addSubcommand((sub) => 
+            sub
+            .setName('ban')
+            .setDescription('Ban a user from the server')
+            .addUserOption((option) => option.setName('target').setDescription('The user to ban').setRequired(true))
+            .addStringOption((option) => option.setName('reason').setDescription('Why are you banning this user?').setRequired(true))
+        ),
+
+    async execute(interaction) {
+        const logChannel = interaction.guild.channels.cache.get(settings.channels.moderationLogs);
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+        const allowedRoles = [settings.roles.botOwner, settings.roles.directive, settings.roles.serverOwner, settings.roles.moderator];
+
+        if (!hasPermission(interaction.member, allowedRoles)) {
+            return interaction.editReply(permissionReply());
+        }
+
+        const subcommand = interaction.options.getSubcommand();
+        const targetUser = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason');
+
+        if (subcommand === 'warning') {
+
+            const logChannelEmbed = new EmbedBuilder()
+                .setTitle('User Warned')
+                .setDescription(`${targetUser.tag} has been warned for the following reason:\n\n${reason}`)
+                .setColor(settings.embedColor)
+                .setImage(settings.footerImages.general)
+                .setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+            log = await logChannel.send({ embeds: [logChannelEmbed] });
+
+            const warningEmbed = new EmbedBuilder()
+                .setTitle('Moderation Warning')
+                .setDescription(`You have been warned in ${interaction.guild.name} for the following reason:\n\n${reason}`)
+                .setColor(settings.embedColor)
+                .setImage(settings.footerImages.general)
+                .setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+            try {
+                await targetUser.send({ embeds: [warningEmbed] });
+            } catch (err) {
+                return interaction.editReply(`The warning was issued to ${targetUser.tag} for ${reason}, but I was unable to send them a DM.`); // dm fail
+            }
+            return interaction.editReply(`You have issued a warning to ${targetUser.tag} for ${reason}`);
+        }
+
+        else if (subcommand === 'timeout') {
+
+           // const logChannelEmbed = new EmbedBuilder().setTitle('User Timed Out').setDescription(`${targetUser.tag} has been timed out for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+           // const timeoutEmbed = new EmbedBuilder().setTitle('Moderation Timeout').setDescription(`You have been timed out in ${interaction.guild.name} for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+           // const duration = interaction.options.getString('duration');
+           // const durationMs = parseInt(duration) * 60 * 1000;
+
+            
+          //  try {
+          //      await targetUser.timeout(durationMs, reason);
+          //      await logChannel.send({ embeds: [logChannelEmbed] }).then(async () => {
+          //          try {
+          //              await targetUser.send({ embeds: [timeoutEmbed] });
+//
+          //          } catch (err) {
+           //             return interaction.editReply(`The timeout was issued to ${targetUser.tag} for ${reason}, but I was unable to send them a DM.`); // dm fail
+          //          }
+              //  });
+               // await interaction.editReply(`You have timed out ${targetUser.tag} for ${reason} for a duration of ${duration} minutes.`);
+          //  } catch (err) {
+          //      return interaction.editReply(`There was an error while executing this command:\n\n` + err.message);
+                await interaction.editReply(`This command is currently unavailable while we squash some bugs. :(`);
+            }
+        
+
+        else if (subcommand === 'kick') {
+            const logChannelEmbed = new EmbedBuilder().setTitle('User Kicked').setDescription(`${targetUser.tag} has been kicked for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+            const kickEmbed = new EmbedBuilder().setTitle('Moderation Kick').setDescription(`You have been kicked from ${interaction.guild.name} for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+
+            try {
+                await interaction.guild.members.kick(targetUser.id, reason);
+                await logChannel.send({ embeds: [logChannelEmbed] }).then(async () => {
+
+                    try {
+                        await targetUser.send({ embeds: [kickEmbed] });
+                    } catch (err) {
+                        return interaction.editReply(`The kick was issued to ${targetUser.tag} for ${reason}, but I was unable to send them a DM.`); // dm fail
+                    }
+                });
+
+                await interaction.editReply(`You have kicked ${targetUser.tag} for ${reason}.`);
+            } catch (err) {
+                return interaction.editReply(`There was an error while executing this command:\n\n` + err.message);
+            }
+        }
+
+        else if (subcommand === 'ban') {
+            const logChannelEmbed = new EmbedBuilder().setTitle('User Banned').setDescription(`${targetUser.tag} has been banned for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+            const banEmbed = new EmbedBuilder().setTitle('Moderation Ban').setDescription(`You have been banned from ${interaction.guild.name} for the following reason:\n\n${reason}`).setColor(settings.embedColor).setImage(settings.footerImages.general).setFooter({ text: `Moderator: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+
+            try {
+                await interaction.guild.members.ban(targetUser.id, { reason });
+                await logChannel.send({ embeds: [logChannelEmbed] }).then(async () => {
+                    try {
+                        await targetUser.send({ embeds: [banEmbed] });
+                    } catch (err) {
+                        return interaction.editReply(`The ban was issued to ${targetUser.tag} for ${reason}, but I was unable to send them a DM.`); // dm fail
+                    }
+                });
+
+                await interaction.editReply(`You have banned ${targetUser.tag} for ${reason}.`);
+            } catch (err) {
+                return interaction.editReply(`There was an error while executing this command:\n\n` + err.message);
+            }
+        }
+
+        else {
+            return interaction.editReply("I couldn't find the command you tried to use. Try again?");
+        }
+    }
+};
---
+++ src/commands/interaction/utility.js
@@ -0,0 +1,114 @@
+const { hasPermission, permissionReply } = require('../../utils/permissionChecker');
+const settings = require('../../config/settings');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, PermissionFlagsBits } = require('discord.js');
+const { execute } = require('./moderations');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('utility')
+        .setDescription('Utility commands for the bot')
+        .addSubcommandGroup((group) =>
+            group
+                .setName('server')
+                .setDescription('Server related utility commands')
+                .addSubcommand((subcommand) =>
+                    subcommand
+                        .setName('membercount')
+                        .setDescription('Get the member count of the server')
+                )
+                .addSubcommand((subcommand) => 
+                    subcommand
+                        .setName('info')
+                        .setDescription('Get information about the server')
+                        )
+                .addSubcommand((subcommand) => 
+                    subcommand
+                        .setName('icon')
+                        .setDescription('Get the server icon')
+                )
+            )
+
+                .addSubcommandGroup((group) =>
+            group
+                .setName('user')
+                .setDescription('User related utility commands')
+                .addSubcommand((subcommand) => 
+                    subcommand
+                        .setName('avatar')
+                        .setDescription('Get the avatar of a user')
+                        .addUserOption((option) => 
+                            option
+                                .setName('target')
+                                .setDescription('The user to get the avatar of')
+                                .setRequired(true))
+                        .addStringOption((option) =>
+                            option
+                                .setName('server')
+                                .setDescription("Do you want this user's server or global avatar?")
+                                .addChoices(
+                                    { name: 'Server Avatar', value: 'server' },
+                                    { name: 'Global Avatar', value: 'global' },
+                                )
+                                .setRequired(true))
+                            
+                        )
+                ),
+
+
+
+
+
+        async execute(interaction, client) {
+            const subcommand = interaction.options.getSubcommand();
+            const targetUser = interaction.options.getUser('target');
+            const avatarType = interaction.options.getString('server');
+
+            if (subcommand === 'avatar') {
+                const user = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
+
+                if (avatarType === 'server') {
+                    const avatarUrl = user && user.avatar ? `https://cdn.discordapp.com/guilds/${interaction.guild.id}/users/${targetUser.id}/avatars/${user.avatar}.png?size=1024` : targetUser.displayAvatarURL({ size: 1024 });
+                    return interaction.reply({ content: `${targetUser.tag}'s server avatar: ${avatarUrl}`, flags: MessageFlags.Ephemeral });
+                }
+                else if (avatarType === 'global') {
+                    const avatarUrl = targetUser.displayAvatarURL({ size: 1024 });
+                    return interaction.reply({ content: `${targetUser.tag}'s global avatar: ${avatarUrl}`, flags: MessageFlags.Ephemeral });
+                } else {
+                    return interaction.reply({ content: 'Invalid avatar type specified. Please choose either "server" or "global".', flags: MessageFlags.Ephemeral });
+                }
+            }
+
+            else if (subcommand === 'info') {
+                const { name, memberCount, createdAt, ownerId } = interaction.guild;
+                const owner = await interaction.guild.members.fetch(ownerId).catch(() => null);
+
+                const infoEmbed = new EmbedBuilder()
+                    .setTitle(`${name}`)
+                    .setDescription('We were created to provide a realistic roleplay experience for government systems for all players to enjoy. We strive to create a welcoming and inclusive community.')
+                    .addFields(
+                        { name: 'Member Count', value: `${memberCount}`, inline: true },
+                        { name: 'Created At', value: `${createdAt.toDateString()}`, inline: true },
+                        { name: 'Owner', value: owner ? owner.user.tag : 'Unknown', inline: true },
+                    )
+                    .setColor(settings.embedColor)
+                    .setThumbnail(interaction.guild.iconURL({ size: 1024 }));
+                return interaction.reply({ embeds: [infoEmbed], flags: MessageFlags.Ephemeral });
+            }
+
+            else if (subcommand === 'membercount') {
+                const memberCount = interaction.guild.memberCount;
+                return interaction.reply({ content: `We currently have ${memberCount} members.`, flags: MessageFlags.Ephemeral });
+            }
+
+            else if (subcommand === 'icon') {
+                const iconUrl = interaction.guild.iconURL({ size: 1024 });
+
+                if (iconUrl) {
+                    return interaction.reply({ content: `Server icon: ${iconUrl}`, flags: MessageFlags.Ephemeral });
+                } else {
+                    return interaction.reply({ content: "I couldn't find this server's icon. Are you sure it has one?", flags: MessageFlags.Ephemeral });
+                }
+            }
+        }
+        
+};
